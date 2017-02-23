474c4eee9b243009a3b85a350774c6a46994ab27
w1reden <wireden@yandex.ru>
2017-02-23 21:37:31 +0200

    Added mongodb support with mongoose.js

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ import express from 'express';
 import config from './config';
 import { join } from 'path';
 import errorHandler from './middlewares/error-handler';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.db, (err) => {
+  if (err) throw err;
+
+  console.log('Database connected');
+});
 
 app.use(express.static(join(__dirname, '/public')));
 
